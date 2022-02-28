7d8d4980c78213e19ea8d17b8d91623428a0b274
Lux <fails9503@gmail.com>
2022-02-28 23:41:32 +0900

    1.2 회원 도메인 실행과 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // 왼쪽은 역할에 의존, 오른쪽은 구현에 이존
+    // OCP, DIP 위반...
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
