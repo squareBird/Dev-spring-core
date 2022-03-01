68052246c8a4ea05448c51e8947e5d50b39473dd
Lux <fails9503@gmail.com>
2022-03-01 10:00:55 +0900

    1.6 구현객체 의존 제거 - private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); - 이렇게하면 new RateDiscountPolicy() 즉 구현에 의존하던 부분은 빼고 역할에만 의존함 - 그냥 이렇게 하면 NullPointException - 누군가가 OrderServiceImpl 클래스의 discountPolicy 객체에 구현 객체를 대신 생성하고 주입해 주어야 함

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,7 +10,11 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // 이렇게하면 new RateDiscountPolicy() 즉 구현에 의존하던 부분은 빼고 역할에만 의존함
+    // 그냥 이렇게 하면 NullPointException
+    // 누군가가 OrderServiceImpl 클래스의 discountPolicy 객체에 구현 객체를 대신 생성하고 주입해 주어야 함
+    private DiscountPolicy discountPolicy;
 
     // order은 멤버나 할인에 대해 모름 그냥 인터페이스에 넘김
     @Override
