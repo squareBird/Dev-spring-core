0b41be1b4b8fade95a778428acfb5ff09f122ed2
Lux <fails9503@gmail.com>
2022-03-01 10:51:39 +0900

    1.9 할인 정책 변경 - 다른 아무것도 변경하지 않아도 된다 - AppConfig에서 할인 정책 구현 부분만 수정해주면 된다

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,7 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
+import hello.core.discount.DiscountPolicyImpl;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -12,6 +13,8 @@ import hello.core.order.OrderServiceImpl;
 // Appconfig를 역할이 드러나게 변경
 // 역할과 구현 클래스가 한 눈에 들어옴, 어플리케이션 전체 구성을 빠르게 파악할 수 있음
 // 코드의 중복 제거
+// 구현영역과 사용영역이 완전하게 분리됨
+// 구현은 AppConfig에서 다 해주고 실제 사용은 OrderServiceImpl, MemberServiceImpl 등에서 함
 public class AppConfig {
 
     // 멤버 서비스 역할
@@ -31,6 +34,7 @@ public class AppConfig {
 
     // 할인 역할
     private DiscountPolicy discountPolicy() {
+//        return new DiscountPolicyImpl();
         return new RateDiscountPolicy();
     }
 
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -21,7 +21,7 @@ public class OrderApp {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order ordr = orderService.createOrder(1L, "iteamA", 10000);
+        Order ordr = orderService.createOrder(1L, "iteamA", 20000);
 
         System.out.println("order = " + ordr.toString());
 
