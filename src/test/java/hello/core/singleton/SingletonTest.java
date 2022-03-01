8cdcf64cfa0eb66a8b3e2a731230f1679baa9e1f
Lux <fails9503@gmail.com>
2022-03-02 08:54:40 +0900

    3.3 스프링 컨테이너를 통한 싱글톤 테스트

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -41,5 +43,27 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
         // same ==
         // equals equals
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+//        // appconfig할때 달라고 할때마다 새로 생성해서 줌
+//        // 1. 조회 : 호출할 때 마다 객체를 생성
+//        MemberService memberService1 = appConfig.memberService();
+//        // 2. 조회 : 호출할 때 마다 객체를 생성
+//        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 다른것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
