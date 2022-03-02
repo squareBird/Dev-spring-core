d856dbdf630d7946eccb35a4ae6f49fd85a4fff4
Lux <fails9503@gmail.com>
2022-03-02 10:53:12 +0900

    4.2 탐색 위치와 기본 스캔 대상 - @Controller : 스프링 MVC 컨테이너로 인식 - @Repository : 스프링 데이터 접근 계층으로 인식하고, 데이터 계층의 예외를 스프링 예외로 변환 - @Configuration : 스프링 설정 정보로 인식, 스프링 빈이 싱글톤을 유지ㅣ하도록 추가 처리 - @Service : 특별한 처리는 하지 않음, 개발자들이 핵심 비즈닉스 로직이 여기 있다는 것을 인식하는데 도움이 됨(비즈니츠 계층 인식 도움이 됨)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( // -> @Component 어노테이션이 붙은걸 찾아서 자동으로 스프링 빈으로 등록해주는 기능
+//        basePackages = "hello.core.member", << 이걸로하면 member폴더만 등록됨
+        basePackages = "hello.core", // 어디서부터 찾는지 지정, 이거 안해주면 온갖 라이브러리까지 다 뒤짐
+        basePackageClasses = AutoAppConfig.class, // AutoAppConfig 클래스의 맨 위의 package부터 스캔
+        // basePackage 설정 안하면 컴포넌트 스캔이 있는 클래스의 패키지가 시작위치가됨
+        // 패키지 위치를 지정하지 않고 설정정보 클래스의 위치를 프로젝트 최상단에 두는 것이 권장
+        // 스프링 부트 @SpringBootApplication에 ComponentScan이 있어서 따로 컨피그 안만듬
         // appConfig를 안읽어오려고 필터
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
