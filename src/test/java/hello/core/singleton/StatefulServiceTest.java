ac07a6cadb82e3fe15cec5f624c562fcb2ff1909
Lux <fails9503@gmail.com>
2022-03-02 09:07:39 +0900

    3.4. 싱글톤 방식의 주의점 - 여러 클라이언트가 하나의 객체 인스턴스를 공유하므로 상태를 유지하게 설계하면 안됨 - 특정 클라이언트에 의존적인 필드가 있으면 안됨 - 특정 클라이언트가 값을 변경할 수 있는 필드가 있으면 안됨 - 가급적 읽기만 가능해야 함 - 필드 대신에 자바에서 공유되지 않는 지역변수 파라미터 스레드로컬 등을 사용해야 함 - 스프링 빈의 필드에 공유 값을 설정하면 큰 장애가 발생할 수 있음

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,40 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //ThreadA : A 사용자 1만원 주문
+        int userAPrice = statefulService1.order("userA", 10000);
+        //ThreadB : B 사용자 2만원 주문
+        //특정 클라이언트가 공유되는 값의 필드를 변경해버림
+        int userBPrice = statefulService1.order("userA", 20000);
+
+        // ThreadA : 사용자 A 주문 금액 조회
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
+
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
