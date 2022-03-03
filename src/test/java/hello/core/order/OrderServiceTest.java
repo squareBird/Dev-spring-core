a8b8c7ad1754d945e267c58206ca76f0e5d0ec6d
Lux <fails9503@gmail.com>
2022-03-03 19:29:17 +0900

    5.1.2 의존관계 주입 Autowired는 주입할 대상이 없으면 오류가 발생함 없어도 오류 안발생하게 해주려면 required=false - 생성자 주입   불변, 필수에 주로 사용   생성자 부분에 의존성 주입해야되는걸 호출해야함   빈이 등록되면서 의존성도 같이 주입됨 - 수정자 주입   생성자로 먼저 -> 그다음 수정자인데 순서 보장X   선택적인, 변경 가능한것에 주로 사용   Autowired의 required옵션으로 선택적으로 - 필드주입   의존관계를 필드에다가 그냥 넣어주나 권장하지 않음   외부에서 변경이 불가능해서 테스트 하기 힘들다는 치명적인 단점이 있음   스프링이 없으면 순수한 자바로 테스트 할 수 있는 방법이 없다...(DI 프레임워크 없으면 아무것도 못함)   fieldInjectionTest 메소드에 설명 참조   결국 setter를 만들어줘야함.. 그럴거면 왜쓰나   결론 : 쓰지말자   하지만, 테스트코드 할때나 쓰자   또, Configuration 클래스는 어차피 스프링에서 쓰니까 거기서나 쓰자  -
    * 자바빈 프로퍼티
    필드를 private로 선언하고 set, get 메소드를 통해서 값을 읽거나 수정하도록!

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -8,6 +8,7 @@ import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,6 +39,17 @@ class OrderServiceTest {
 
         // then
         assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+    @Test
+    void fieldInjectionTest() {
+        // 필드주입으로 완성하고 이렇게 스프링이 없는 환경에서 테스트를 진행할수도 있다
+        // 지금 이렇게만 해두면 스프링이 없기때문에 Autowired가 동작하지 않아 의존성이 주입되지 않았다.
+        // private이라서 외부에서 orderservice.memberRepository 이런식으로 넣어줄수도 없다.
+        // 결국 setter를 또 추가로 만들어줘야함...
+        OrderServiceImpl orderService = new OrderServiceImpl();
+        // createOrder는 memberRepository와 discountPolicy가 필요한데 두개의 의존성이 주입되지 않았으니 nullpointException이 발생한다
+        orderService.createOrder(1L, "itema", 10000);
     }
 
 
