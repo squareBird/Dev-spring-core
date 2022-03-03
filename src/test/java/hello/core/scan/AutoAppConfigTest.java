24c68587184d2061cb44ea341ac46e5be99e3014
Lux <fails9503@gmail.com>
2022-03-03 19:49:10 +0900

    궁금증 Test에서 Spring이 동작하는 이유..2

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -16,6 +16,9 @@ public class AutoAppConfigTest {
         // 여기서 왜 스프링이 올라가는걸까?
         // new AnnotationConfigApplicationContext(AutoAppConfig.class); 때문일까?
         // AutoAppConfig에 Configuration과 Component Scan이 있어서인가
+        // 일단 AppConfig에 Component Scan을 빼버리면
+        // AnnotationConfigApplicationContext을 통해 기본적으로 사용하는 빈과 AutoAppConfig 까지만 빈으로 올라가고
+        // memberService나 OrderService, MemberRepository, DiscountPolicy는 Bean으로 올라가지 않는다!
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
         MemberService memberService = ac.getBean(MemberService.class);
