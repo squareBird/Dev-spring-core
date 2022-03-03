d1d2ac78795267b54aa9f9a6f9fa2b51e59010e3
Lux <fails9503@gmail.com>
2022-03-03 19:32:33 +0900

    5.1.2 의존관계 주입 Autowired는 주입할 대상이 없으면 오류가 발생함 없어도 오류 안발생하게 해주려면 required=false - 생성자 주입   불변, 필수에 주로 사용   생성자 부분에 의존성 주입해야되는걸 호출해야함   빈이 등록되면서 의존성도 같이 주입됨 - 수정자 주입   생성자로 먼저 -> 그다음 수정자인데 순서 보장X   선택적인, 변경 가능한것에 주로 사용   Autowired의 required옵션으로 선택적으로 - 필드주입   의존관계를 필드에다가 그냥 넣어주나 권장하지 않음   외부에서 변경이 불가능해서 테스트 하기 힘들다는 치명적인 단점이 있음   스프링이 없으면 순수한 자바로 테스트 할 수 있는 방법이 없다...(DI 프레임워크 없으면 아무것도 못함)   fieldInjectionTest 메소드에 설명 참조   결국 setter를 만들어줘야함.. 그럴거면 왜쓰나   결론 : 쓰지말자   하지만, 테스트코드 할때나 쓰자   또, Configuration 클래스는 어차피 스프링에서 쓰니까 거기서나 쓰자  - 일반 메소드 주입    그냥 아무메소드에나 Autowired 달아도 됨    잘 안씀
     ★ 스프링 컨테이너가 관리하는 스트링 빈만 Autowired 동작함. Member같은데서 Autowired 백날 달아도 소용이 없음
     ★ 스프링 컨테이너 등록 대상
        1. Configration파일에 @Bean
        2. @Component 어노테이션
     ★ Autowired는 Bean을 자동으로 의존성 주입!
    
    * 자바빈 프로퍼티
    필드를 private로 선언하고 set, get 메소드를 통해서 값을 읽거나 수정하도록!

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,32 +12,25 @@ import org.springframework.stereotype.Component;
 @Component // ("service")
 public class OrderServiceImpl implements OrderService{
 
-    @Autowired private MemberRepository memberRepository;
+    private MemberRepository memberRepository;
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     // 이렇게하면 new RateDiscountPolicy() 즉 구현에 의존하던 부분은 빼고 역할에만 의존함
     // 그냥 이렇게 하면 NullPointException
     // 누군가가 OrderServiceImpl 클래스의 discountPolicy 객체에 구현 객체를 대신 생성하고 주입해 주어야 함
-    @Autowired private DiscountPolicy discountPolicy;
+    private DiscountPolicy discountPolicy;
 
-//    @Autowired(required = false)
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        System.out.println("memberRepository = " + memberRepository);
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    // Autowired 주석처리하면 setDiscountPolicy부분이 사용하는데가 없어서 회색으로 변함
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        System.out.println("discountPolicy = " + discountPolicy);
-//        this.discountPolicy = discountPolicy;
-//    }
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("1. OrderServiceImpl.OrderServiceImpl 생성자!");
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        System.out.println("1. OrderServiceImpl.OrderServiceImpl 생성자!");
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    // 일반 메소드 주입
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     // order은 멤버나 할인에 대해 모름 그냥 인터페이스에 넘김
     @Override
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -41,16 +41,16 @@ class OrderServiceTest {
         assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 
-    @Test
-    void fieldInjectionTest() {
-        // 필드주입으로 완성하고 이렇게 스프링이 없는 환경에서 테스트를 진행할수도 있다
-        // 지금 이렇게만 해두면 스프링이 없기때문에 Autowired가 동작하지 않아 의존성이 주입되지 않았다.
-        // private이라서 외부에서 orderservice.memberRepository 이런식으로 넣어줄수도 없다.
-        // 결국 setter를 또 추가로 만들어줘야함...
-        OrderServiceImpl orderService = new OrderServiceImpl();
-        // createOrder는 memberRepository와 discountPolicy가 필요한데 두개의 의존성이 주입되지 않았으니 nullpointException이 발생한다
-        orderService.createOrder(1L, "itema", 10000);
-    }
+//    @Test
+//    void fieldInjectionTest() {
+//        // 필드주입으로 완성하고 이렇게 스프링이 없는 환경에서 테스트를 진행할수도 있다
+//        // 지금 이렇게만 해두면 스프링이 없기때문에 Autowired가 동작하지 않아 의존성이 주입되지 않았다.
+//        // private이라서 외부에서 orderservice.memberRepository 이런식으로 넣어줄수도 없다.
+//        // 결국 setter를 또 추가로 만들어줘야함...
+//        OrderServiceImpl orderService = new OrderServiceImpl();
+//        // createOrder는 memberRepository와 discountPolicy가 필요한데 두개의 의존성이 주입되지 않았으니 nullpointException이 발생한다
+//        orderService.createOrder(1L, "itema", 10000);
+//    }
 
 
 }
