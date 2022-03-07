1de94fe700e373b2722ec00d811ed2d81412f145
Lux <fails9503@gmail.com>
2022-03-07 21:09:18 +0900

    5.7 조회한 빈이 모두 필요할 때(List, Map) - 해당 타입의 스프링 빈이 다 필요한 경우도 있다 - 예를들어 할인 서비스가 있는데 클라이언트가 rate, fix를 선택할 수 있을 경우 1. DiscountService는 DiscountPolicy들을 Map으로 받음 2. key는 bean 이름, DiscountPolicy는 bean인듯 3. discountCode 즉 bean 이름을 파라미터로 받음 4. DiscountPolicy에는 policyMap에서 discountCode 즉 key값으로 get을 해서 rate 또는 fix 할인 정책을 가져옴 5. 가져온 정책에 따라 discount 명령 수행

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,57 @@
+package hello.core.autowired;
+
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        // 1. DiscountService는 DiscountPolicy들을 Map으로 받음
+        // 2. key는 bean 이름, DiscountPolicy는 bean인듯
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        // 3. discountCode 즉 bean 이름을 파라미터로 받음
+        // 4. DiscountPolicy에는 policyMap에서 discountCode 즉 key값으로 get을 해서 rate 또는 fix 할인 정책을 가져옴
+        // 5. 가져온 정책에 따라 discount 명령 수행
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
+
+}
