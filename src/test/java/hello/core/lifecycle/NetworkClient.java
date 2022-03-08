a28e3493654814bf5a42a3d0afc6332d32edccc0
Lux <fails9503@gmail.com>
2022-03-08 19:49:05 +0900

    6.2 초기화 소멸 인터페이스 - 이 인터페이스는 스프링 전용 인터페이스로, 해당 코드가 스프링 전용 인터페이스에 의존적으로 됨 - 초기화, 소멸 메서드의 일믕르 변경할 수 없음 - 내가 코드를 고칠 수 없는 외부 라이브러리에 적용할 수 없음
    이건 초창기에 나온 방법 최근엔 거의 사용하지 않음

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -27,4 +28,15 @@ public class NetworkClient {
    public void disconnect() {
         System.out.println("close");
     }
+   // 의존관계 주입후에 해당 메소드 호출
+   @Override
+   public void afterPropertiesSet() throws Exception {
+       connect();
+       call("초기화 연결 메시지");
+   }
+
+   @Override
+   public void destroy() throws Exception {
+       disconnect();
+   }
 }
