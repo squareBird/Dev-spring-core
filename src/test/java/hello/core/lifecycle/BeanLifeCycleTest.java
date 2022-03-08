a8463d95708b13b659f544c35e608782a33f14d8
Lux <fails9503@gmail.com>
2022-03-08 19:44:57 +0900

    6.1 빈 생명주기 콜백 - DB 커넥션 풀, 네트워크 소켓 등 애플리케이션 시작시 필요한 연결을 미리 해두고, 애플리케이션 종료 시점에 연결을 모두 종료하는 작업을 진행하려면 객체와 촉히ㅘ와 종료 작업이 필요하다. - 스프링 빈 라이프 사이클은 다음과 같다   (생성자는 객체 생성할때 같이 들어감)   ㅇ 객체 생성 -> 의존관계 주입 - 스프링 빈은 객체를 생성하고 의존관계 주입이 끝난 뒤에야 데이터를 사용할 수 있는 준비가 완료 됨. - 따라서 초기화 작업은 의존관계 주입이 모두 완료되고 호출해야 함. 그런데 개발자가 어떻게 의존관계 주입이 모두 완료된 시점을 알 수 있을까? - 스프링은 의존관계 주입이 완료되면 스프링 빈에게 콜백 메서드를 통해서 초기화 시점을 알려주는 다양한 기능을 제공, 또한 스프링 컨테이너가 종료되기 직전 소멸 콜백을 줌 - 스프링빈 이벤트 라이프 사이클 스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료
    초기화 콜백 : 빈이 생성된 후, 의존관계 주입이 오나료된 후
    소멸전 콜백 : 빈이 소멸되기 직전
    
    객체의 생성과 초기화를 분리하자
    - 생성자는 필수 정보를 받고 메모리를 할당해서 객체를 생성하는 책임
    - 초기화는 이렇게 생성된 값들을 활용해서 외부 커넥션을 연결하는 등 무거운 작업을 수행
    - 생성자 안에서 무거운 초기화 작업을 함께 하기보다는 객체를 생성하는 부분과 초기화 하는 부분을 명확하게 나누는 것이 유지보수 괁넘에서 좋음
    - 초기화 작업이 내부 값들만 약간 변경하는 정도로 단순한 경우 생성자에서 한번에 다 처리하는게 나을수도 있음
    
    - 외부 커넥션 연결 등을 최초의 연결 시도시까지 미룰 수 있음(동작을 조금 지연시킬 수 있음)

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,32 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            // 생성자에서 set하기 전이므로 일단 객체 생성만 된 상태
+            /* 생성자 호출, url = null
+            connect = null
+            call = null messgae = 초기화 연결 메시지 */
+            networkClient.setUrl("https://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
