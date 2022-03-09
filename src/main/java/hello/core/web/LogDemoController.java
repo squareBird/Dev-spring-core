f81b3af57b8f49fad80ad502a5c713f975597b3d
Lux <fails9503@gmail.com>
2022-03-09 13:22:52 +0900

    7.6 웹 스코프와 프로바이더 ★ ObjectProvider덕분에 getObject를 호출하는 시점까지 request scope의 빈의 생성을 지연할 수 있음 - 1. getObject 메소드가 동작하시는 시점에 생성됨 - 2. uuid 할당 - 3. 로직 돌때 로그 찍힘

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +17,17 @@ public class LogDemoController {
     private final LogDemoService logDemoService;
     // 스프링 처음에 실행할때 에러뜸
     // myLogger을 주입 받아야하는데 myLogger의 scope가 reuqest여서 스프링이 뜨는 시점에는 아직 MyLogger가 없음 -> Provider로 해결가능
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
-        String requestURL = request.getRequestURI().toString();
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
