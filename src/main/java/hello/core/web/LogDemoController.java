2eef08a2b78ab2a80e33ba95faaad1e4e263e922
Lux <fails9503@gmail.com>
2022-03-09 13:31:29 +0900

    7.6 스코프와 프록시 - proxyMode = ScopedProxyMode.???? - 대상이 클래스면 TARGET_CLASS - 인터페이스면 Interface
    동작정리
    1. CGLIB 라이브러리로 내 클래스를 상속받은 가짜 프록시 객체를 만들어서 주입
    2. 가짜 프록시 객체는 실제 요청이 오면 그때 내부에서 실제 빈을 요청하는 위임 로직이 있음
    3. 가짜 프록시 객체는 실제 request scope와는 관계 없음, 내부에 단순한 위임 로직만 있고, 싱글톤처럼 동작한다
    
    특징
    - 프록시 덕분에 클라이언트는 싱글톤 빈을 사용하듯이 편리하게 request scope를 사용할 수 있음
    ★ Provider를 사용하든 프록시를 사용하든 핵심 아이디어는 진짜 객체 조회를 꼭 필요한 시점까지 지연처리한다는 것
    ★ 단지 애노테이션 설정 변경만으로 원본 객체를 프록시 객체로 대체할 수 있다. 이것이 바로 다형성과 DI 컨테이너가 가진 가장 큰 강점
    - 꼭 웹 스코프가 아니어도 프록시는 사용할 수 있음
    
    주의점
    - 마치 싱글톤 처럼 사용하는 것 같지만 다르므로 주의
    - 이런 특별한 스코프는 필요한 곳에서 최소한으로만 사용해야 함

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -17,13 +17,21 @@ public class LogDemoController {
     private final LogDemoService logDemoService;
     // 스프링 처음에 실행할때 에러뜸
     // myLogger을 주입 받아야하는데 myLogger의 scope가 reuqest여서 스프링이 뜨는 시점에는 아직 MyLogger가 없음 -> Provider로 해결가능
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+
+        // myLogger = class hello.core.common.MyLogger$$EnhancerBySpringCGLIB$$260621e1 이라고 나옴
+        // 내가 만든 마이그로거가 아닌 껍데기가 생겨서 돌아가는 것
+        // 스프링 컨테이너에 myLogger라는 이름으로 이 가짜 프록시 객체를 등록
+        System.out.println("myLogger = " + myLogger.getClass());
+
+        // myLogger의 메소드를 호출하면 진짜 myLogger의 메소드를 찾아서 실행해줌
+        // 가짜 프록시 안에는 진짜 myLogger를 찾는 방법을 알고 있음음
+        // 가짜 프록시객체는 원본 클래스를 상속 받아서 만들어져서 클라이언트 입장에서는 이게 진짠지 아닌지 알 필요가 없음(다형성)
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
